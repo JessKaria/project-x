adbc6e48df72a36db7d88300520bb65fe316bc57
Jess <jesskaria1@gmail.com>
2021-02-17 20:52:58 +0000

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,9 @@ const Footer = () => {
 
   return <footer className="footer">
     <div className="content has-text-centered">
-      <p>
-        <h5>Brought to you by Jess Karia</h5>
-        <button>CONNECT WITH ME ON LINKEDIN</button>
-      </p>
+
+      <p>Brought to you by Jess Karia</p>
+
     </div>
   </footer>
 }
